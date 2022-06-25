d7b3556d3b5243e4daec7a8e9047b71883c361de
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-06-25 11:33:57 +0800

    setup devise layout, controllers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   # get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
   # Defines the root path route ("/")
   root "static_pages#home"
 end
