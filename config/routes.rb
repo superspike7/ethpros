0d13390de64ffdd3bc62ab58cdc6f8175c85e965
Spike Vinz <spikevinz@gmail.com>
2022-06-30 19:46:13 +0800

    hotfix git conflict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  # get 'static_pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-
   resources :stocks, only: [:index]
-=======
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     passwords: 'users/passwords',
     confirmations: 'users/confirmations'
   }
->>>>>>> setup devise layout, controllers, and views
-  # Defines the root path route ("/")
+
   root "stocks#index"
 end
