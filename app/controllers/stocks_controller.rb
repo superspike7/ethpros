6efe8ba6d1cdb7b28b64c33e5d4568bd310c390c
Spike Vinz <spikevinz@gmail.com>
2022-07-07 09:48:56 +0800

    add index action for stocks portfolio with eager loading

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,5 +3,7 @@ class StocksController < ApplicationController
 
   def index
     redirect_to home_path unless user_signed_in?
+
+    @transactions = current_user.stock_transactions.includes(:stock)
   end
 end
