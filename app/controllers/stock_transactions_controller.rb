c6607961ae5a86c9f9c7a468359b078a0709e7b3
Spike Vinz <spikevinz@gmail.com>
2022-07-07 08:15:59 +0800

    add stock transactions controller with buying implementation

---
+++ app/controllers/stock_transactions_controller.rb
@@ -0,0 +1,22 @@
+class StockTransactionsController < ApplicationController
+  def new
+    @stock = Stock.find_by_symbol(params[:symbol])
+    @new_stock = StockTransaction.new(stock_id: @stock.id)
+  end
+
+  def create
+    @stock_transaction = current_user.stock_transactions.new(stock_transaction_params)
+
+    if @stock_transaction.save
+      redirect_to stocks_path, notice: "You have successfully bought a stock"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def stock_transaction_params
+    params.require(:stock_transaction).permit(:amount, :stock_id)
+  end
+end
