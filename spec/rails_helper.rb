a8ff37e6393e967a78d9bb1fefcc3046b82eb06a
Spike Vinz <spikevinz@gmail.com>
2022-07-07 09:46:56 +0800

    refactor factories to a separate folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rails'
 require 'support/factory_bot'
+require 'support/database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,7 +39,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.include Devise::Test::IntegrationHelpers, type: :feature
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
