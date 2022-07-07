ed2abbf638125c519e49c2233dbd804345444f3b
Spike Vinz <spikevinz@gmail.com>
2022-07-07 11:24:03 +0800

    fix when user is not loggedin on index action

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,8 +2,10 @@ class StocksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    redirect_to home_path unless user_signed_in?
-
-    @transactions = current_user.stock_transactions.includes(:stock)
+    unless user_signed_in?
+      redirect_to home_path 
+    else
+      @transactions = current_user.stock_transactions.includes(:stock)
+    end
   end
 end
