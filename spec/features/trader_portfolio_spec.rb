0c7085fc8e2c0049954392cf3ac6c779dfc8624c
Spike Vinz <spikevinz@gmail.com>
2022-07-07 09:47:26 +0800

    add feature spec for trader portfolio page

---
+++ spec/features/trader_portfolio_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "TraderPortfolio", type: :feature do
+  scenario 'Trader Bought a stock ' do
+    transaction = create(:stock_transaction)
+    sign_in transaction.user 
+
+    visit 'stocks'
+
+    expect(page).to have_content("AAPL")
+    expect(page).to have_content("69")
+  end
+end
