de9657e8df59550a12ced22fb69de6b2de6e0c6b
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-07-10 21:45:08 +0800

    create routes for admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,25 @@ class AdminController < ApplicationController
   layout 'admin'
   
   def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+  
+  def all
+    render :action => 'all'
+  end
+  
+  def pending
+  end
+  
+  def transactions
   end
 end
