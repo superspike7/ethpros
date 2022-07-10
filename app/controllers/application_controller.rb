df723111dfe0ce76ccfd2f5601b68f4cbecdd94e
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-07-11 00:59:22 +0800

    create layout for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.admin?
-      admin_path
+      admin_index_path
     else current_user.user?
       stocks_path
     end
