2db4bda7a98a7134f26cb8116559445605badaa8
Spike Vinz <spikevinz@gmail.com>
2022-07-11 11:16:22 +0800

    add selling stocks implementation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,15 +2,23 @@ class Transaction < ApplicationRecord
   belongs_to :stock
   belongs_to :user
   
-  after_create :add_shares
+  after_create :update_shares
 
   private
 
-  def add_shares
+  def update_shares
     if self.stock_id
       share = self.user.shares.find_or_create_by(stock_id: self.stock_id)
-      share.value = self.amount + share.value
+      share.value = changed_share_value(share.value)
       share.save!
+    end
+  end
+
+  def changed_share_value(value)
+    if self.transaction_type == 'SELL'
+      value - self.amount 
+    elsif self.transaction_type == 'BUY'
+      value + self.amount 
     end
   end
 end
