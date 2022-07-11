587673c8cbd77f1fb0c45e01edd16cec2183a06a
Spike Vinz <spikevinz@gmail.com>
2022-07-12 07:37:04 +0800

    refactor backfillingstock job to have only important data

--- app/jobs/backfill_stocks_job.rb
+++ app/jobs/backfill_stocks_job.rb
@@ -4,7 +4,14 @@ class BackfillStocksJob < ApplicationJob
   def perform(*args)
     client = IEX::Api::Client.new
     client.stock_market_list(:iexvolume, {listLimit: 20000}).each do |data|
-      Stock.create(data)
+      Stock.create(
+        symbol: data.symbol,
+        company_name: data.company_name,
+        price: data.latest_price,
+        change_percent: data.change_percent_s,
+        logo_url: "https://storage.googleapis.com/iex/api/logos/#{data.symbol}.png",
+        chart: client.chart(data.symbol)
+      )
     end
   end
 end
