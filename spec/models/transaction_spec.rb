223e70040c53af0dbfeff72428b41e25772b23a6
Spike Vinz <spikevinz@gmail.com>
2022-07-11 11:31:38 +0800

    add unit test for transaction call back logic

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  describe "add shares callback" do
+  describe "after create callback" do
     it 'creates a share after transaction is created' do
       transaction = create(:transaction, amount: 10)
 
       expect(transaction.user.shares.last).to be_present
       expect(transaction.user.shares.last.value).to be(10)
+    end
+    
+    it 'adds an existing share after transaction is created' do
+      user = create(:user, email: "spike@gmail.com")
+      create(:transaction, user_id: user.id)
+      user.transactions.create(stock_id: Stock.first.id, amount: 10, transaction_type: 'BUY')
+
+      expect(user.shares.last).to be_present
+      expect(user.shares.last.value).to be(79)
+    end
+    
+    it 'subtracts an existing share after transaction is created' do
+      user = create(:user, email: "spike@gmail.com")
+      create(:transaction, user_id: user.id)
+      user.transactions.create(stock_id: Stock.first.id, amount: 10, transaction_type: 'SELL')
+
+      expect(user.shares.last).to be_present
+      expect(user.shares.last.value).to be(59)
     end
   end
 end
