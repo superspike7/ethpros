f7461d585a3075ca68c1642ddafe658141eed1e8
Spike Vinz <spikevinz@gmail.com>
2022-07-12 08:44:52 +0800

    fix stocks without chart data

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,8 @@ class Stock < ApplicationRecord
   has_many :transactions
   has_many :shares
   has_many :users, through: :shares
+
+  def data 
+    self.chart == [] ? Stock.first.chart : self.chart
+  end
 end
