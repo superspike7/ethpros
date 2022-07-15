d3d6327f5d329c00e945e7a2e4674f3044e1a91e
Spike Vinz <spikevinz@gmail.com>
2022-07-15 13:26:34 +0800

    refactor buy and sell shares to affect user balance

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   describe "after create callback" do
-    it 'creates a share after transaction is created' do
-      transaction = create(:transaction, amount: 10)
-
-      expect(transaction.user.shares.last).to be_present
-      expect(transaction.user.shares.last.value).to be(10)
+    context 'Trader Buys a Stock' do
+      it 'adds an existing share and updates user balance after transaction is created' do
+        user = create(:user, balance: 100)
+        stock = create(:stock, price: 12)
+        user.transactions.create(stock_id: stock.id, amount: 2, transaction_type: 'buy')
+        
+        expect(user.shares.last).to be_present
+        expect(user.shares.last.value).to eq(2)
+        expect(user.balance).to eq(76)
+      end
     end
     
-    it 'adds an existing share after transaction is created' do
-      user = create(:user, email: "spike@gmail.com")
-      create(:transaction, user_id: user.id)
-      user.transactions.create(stock_id: Stock.first.id, amount: 10, transaction_type: 'buy')
+    context 'Trader Sells a Stock' do
+      it 'subtracts an existing share and updates user balancke after transaction is created' do
+        user = create(:user, email: "spike@gmail.com", balance: 45)
+        stock = create(:stock, price: 12)
+        create(:transaction, user_id: user.id, amount: 10)
+        user.transactions.create(stock_id: Stock.first.id, amount: 5, transaction_type: 'sell')
 
-      expect(user.shares.last).to be_present
-      expect(user.shares.last.value).to be(79)
-    end
-    
-    it 'subtracts an existing share after transaction is created' do
-      user = create(:user, email: "spike@gmail.com")
-      create(:transaction, user_id: user.id)
-      user.transactions.create(stock_id: Stock.first.id, amount: 10, transaction_type: 'sell')
-
-      expect(user.shares.last).to be_present
-      expect(user.shares.last.value).to be(59)
+        expect(user.shares.last).to be_present
+        expect(user.shares.last.value).to eq(5)
+        expect(user.balance).to eq(105)
+      end
     end
   end
 end
