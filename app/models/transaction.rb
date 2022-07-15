94e239cc1a77fc13c31523974aacce75f3bd12f8
Spike Vinz <spikevinz@gmail.com>
2022-07-15 14:05:18 +0800

    refactor transaction to update deposit and withdraw

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,18 +4,29 @@ class Transaction < ApplicationRecord
   belongs_to :stock, optional: true
   belongs_to :user
 
-  after_create :update_shares
+  after_create :update_user_shares_and_balance
 
   private
 
-  def update_shares
+  def update_user_shares_and_balance
     if stock_id
       share = user.shares.find_or_create_by(stock_id:)
+      share.value = changed_share_value(share.value)
       user.balance = calculate_balance(share.stock.price)
       user.save!
-      share.value = changed_share_value(share.value)
       share.save!
+    else
+      update_user_balance
     end
+  end
+
+  def update_user_balance
+    if transaction_type == 'deposit'
+      user.balance += amount 
+    elsif transaction_type == 'withdraw'
+     user.balance -= amount 
+    end
+    user.save!
   end
 
   def calculate_balance(price)
@@ -28,7 +39,7 @@ class Transaction < ApplicationRecord
 
   def changed_share_value(value)
     if transaction_type == 'sell'
-      amount - value
+      value - amount
     elsif transaction_type == 'buy'
       amount + value
     end
