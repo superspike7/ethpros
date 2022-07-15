f8cf7bc0539e3c835ff68e563d86cf1dcef75fa0
Spike Vinz <spikevinz@gmail.com>
2022-07-15 11:11:33 +0800

    add user balance migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_005701) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_021122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,8 +63,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_005701) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wallets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
   add_foreign_key "shares", "stocks"
   add_foreign_key "shares", "users"
   add_foreign_key "transactions", "stocks"
   add_foreign_key "transactions", "users"
+  add_foreign_key "wallets", "users"
 end
