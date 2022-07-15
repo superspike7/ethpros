dbfa311bb23cf63aea7d5fef39ad9b5652c9b9de
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-07-15 16:20:09 +0800

    fix pending button and new user button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,18 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      redirect_to admin_all_path,  notice: 'The user has been created.'
     else
-      render :new
+      render :new, alert: @user.errors.first.full_message
     end
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'The user has been updated.'
+    if @user.update(pending: params[:pending])
+      redirect_to admin_all_path, notice: 'The user has been updated.'
+      if user_params.has_key(:pending)
+        UserMailer.with(user: @user).approved_mail.deliver_later
+      end
     else
       render :edit
     end
@@ -44,6 +47,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:pending)
+      params.require(:user).permit(:pending, :email, :password, :password_confirmation)
     end
 end
