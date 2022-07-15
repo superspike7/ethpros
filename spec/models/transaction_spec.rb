eee48e517edf9602cc1bbad8ba1985e429c1c511
Spike Vinz <spikevinz@gmail.com>
2022-07-15 14:05:18 +0800

    refactor transaction to update deposit and withdraw

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -16,14 +16,31 @@ RSpec.describe Transaction, type: :model do
     
     context 'Trader Sells a Stock' do
       it 'subtracts an existing share and updates user balancke after transaction is created' do
-        user = create(:user, email: "spike@gmail.com", balance: 45)
+        user = create(:user, email: "spike@gmail.com", balance: 165)
         stock = create(:stock, price: 12)
-        create(:transaction, user_id: user.id, amount: 10)
-        user.transactions.create(stock_id: Stock.first.id, amount: 5, transaction_type: 'sell')
+        user.transactions.create(stock_id: stock.id, amount: 10, transaction_type: 'buy')
+        transaction = user.transactions.create(stock_id: stock.id, amount: 5, transaction_type: 'sell')
 
-        expect(user.shares.last).to be_present
-        expect(user.shares.last.value).to eq(5)
+        expect(user.shares.first.value).to eq(5)
         expect(user.balance).to eq(105)
+      end
+    end
+    
+    context 'Trader Deposits Cash' do
+      it 'adds the users current balance' do
+        user = create(:user, email: "spike@gmail.com", balance: 45)
+        user.transactions.create(amount: 5, transaction_type: 'deposit')
+
+        expect(user.balance).to eq(50)
+      end
+    end
+    
+    context 'Trader Withdraws Cash' do
+      it 'adds the users current balance' do
+        user = create(:user, email: "spike@gmail.com", balance: 45)
+        user.transactions.create(amount: 40, transaction_type: 'withdraw')
+
+        expect(user.balance).to eq(5)
       end
     end
   end
